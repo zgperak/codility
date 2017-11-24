45da9d8abc1007cbc46ae52952b3d817b58e48cc
tperkovic <tomislav.perkovic@skolskaknjiga.hr>
2017-11-24 15:45:04 +0100

    FIX: identation

--- PassingCars/solution.php
+++ PassingCars/solution.php
@@ -7,25 +7,25 @@
  */
 
 function solution(array $A):int {
-    // write your code in PHP7.0
-    $totalCarsGoingWest = 0;
+	// write your code in PHP7.0
+	$totalCarsGoingWest = 0;
 	foreach($A as $carGoingWest){
 		$totalCarsGoingWest += $carGoingWest; // because going east would be 0
 	}
 
 	$passedWest = 0;
-    $passingCars = 0;
+	$passingCars = 0;
 
-    foreach($A as $carGoingWest){
-        if($carGoingWest){
-            $passedWest++;
-            continue;
-        }
-		$passingCars += ($totalCarsGoingWest - $passingCars);
-        if($passingCars>1000000000) return -1;
+	foreach($A as $carGoingWest){
+		if($carGoingWest){
+			$passedWest++;
+			continue;
+		}
+		$passingCars += ($totalCarsGoingWest - $passedWest);
+		if($passingCars>1000000000) return -1;
 
-    }
+	}
 
-    return $passingCars;
+	return $passingCars;
 
 }
